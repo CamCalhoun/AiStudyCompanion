ccf99adde00764884451607a080f6b0733b1efd9
Jonathan Buckel <jonathanbuckel38@gmail.com>
2025-01-26 14:22:19 -0500

    Jonathan - class-demo-1.py now loads API key from .env file. No errors at this point.

--- backend/class-demo-1.py
+++ backend/class-demo-1.py
@@ -1,5 +1,10 @@
 from openai import OpenAI
-client = OpenAI()
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 completion = client.chat.completions.create(
     model="gpt-4o",
     store=True,
