b641395843a754fa8b549adb396e1785ffbfddbc
unknown <gagemkeslar@gmail.com>
2025-01-27 14:24:54 -0500

    Simple Chatbot added

--- backend/class-demo-1.py
+++ backend/class-demo-1.py
@@ -4,15 +4,42 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-completion = client.chat.completions.create(
-    model="gpt-4o",
-    store=True,
-    messages=[
-        {"role": "user", "content": "write a haiku about ai"}
-    ]
-)
+#help of making the chatbot class comes from Kie Codes: https://www.youtube.com/watch?v=Xujt_rFf9Us
+class Chatbot:
+    def __init__(self, client):
+        self.client = client
+        self.context = [
+            {"role": "system", "content": "You are a silly class clown, be witty and a bit of a jokester"}
+    ]   
 
-print (completion.choices[0].message.content)
-print (completion._request_id)
-print (completion.usage)
+    def chat(self, message:str):
+        self.context.append(
+            {"role":"user","content":message}
+        )
+        response = self.client.chat.completions.create(
+            model = "gpt-4o",
+            store = True,
+            messages = self.context,
+        )
+        
+        response_content = response.choices[0].message.content
+        self.context.append(
+            {"role": "assistant","content": response_content}
+        )
+        print(f'Silly Bot: {response.choices[0].message.content}')
+
+client = OpenAI()
+goofy = Chatbot(client)
+loop = True
+
+print ("Hello! Please talk to silly bot! type q to quit." ,flush=True)
+loop = True
+while loop:
+    user_message = input("User: ")
+    if user_message.lower() == "q":
+        loop = False
+    else:
+        goofy.chat(user_message)
+
+    
+
