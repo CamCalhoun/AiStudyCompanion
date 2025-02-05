568704f1088fce22c3d6fe76f4795e8bfed9f714
Seth Morgan <70118609+Mmmtasty@users.noreply.github.com>
2025-02-05 10:47:02 -0500

    Math.py initial push

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 
+
 class Fruit(BaseModel):
     name: str
 
---
+++ backend/math.py
@@ -0,0 +1,50 @@
+from subject import Subject
+from typing import List, Dict
+
+
+class Math(Subject):
+    # Override subject variables
+    subjectName: str = 'Mathematics'
+    subjectElo: int = 800
+    subjectBreakpoints: List[int] = [0, 800, 1600]
+    topic="algebra"
+    subtopic="solving equations"
+
+    math_topics: Dict[str,List[str]] = {
+        "basic math": ["addition", "subtraction", "multiplication", "division"],
+        "pre-algebra": ["fractions", "decimals", "negative numbers", "order of operations"],
+        "algebra": ["solving equations", "graphing linear equations", "polynomials", "quadratic equations"],
+        "trigonometry": ["sine and cosine", "tangent", "trigonometric identities", "unit circle"],
+        "precalculus": ["limits", "sequences and series", "functions", "conic sections"],
+        "calculus": ["derivatives", "integrals", "chain rule", "limits"],
+        "statistics": ["mean, median, mode", "probability", "standard deviation", "hypothesis testing"],
+        "linear algebra": ["vectors", "matrices", "determinants", "eigenvalues and eigenvectors"]
+    }
+
+    
+    subjectPrompts: Dict[int, str] = {
+        0: 'Create a basic math multiple-choice question related to {topic} for elementary level.',
+        800: 'Create a math multiple-choice question related to {topic} for high school level. Provide the question and options, and wait for the user to respond. If correct, say "Correct"; otherwise, explain why it is incorrect and ask if they want another question.',
+        1600: 'Create an advanced level math multiple-choice question related to {topic} for college level. Provide the question and options, and wait for the user to respond. If correct, say "Correct"; otherwise, explain why it is incorrect and ask if they want another question.'
+    }
+
+        
+    def getPossible_Subtopics(self, topic):
+        if topic in self.math_topics.keys():
+            possible_subtopics = self.math_topics.values(topic)
+            return possible_subtopics
+        else:
+            print("topic not found")
+    
+    def setSubtopic(self, subtopic):
+        if subtopic in self.math_topics.values():
+           self.subtopic=subtopic
+        else:
+            print("subtopic not found")
+            
+    #example current 
+    current_prompt : str = subjectPrompts[subjectElo, subtopic]
+
+
+    
+
