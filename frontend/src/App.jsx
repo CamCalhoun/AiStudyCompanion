1f2e3416227086c8a2438afb9ee9806330be1a5f
Cameron Calhoun <cameron.calhoun01@gmail.com>
2025-02-19 15:32:46 -0500

    Added title and Welcome banner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,24 @@ import './App.css'
 
 function App() {
 
-  return (
-    <>
-      <div className="grid grid-rows-[35.28%_64.72%] h-screen">
-        <div className="grid grid-rows-2 h-full">
-          <div className="bg-white"></div>
-          <div className="bg-pwblue"></div>
-        </div>
-        <div className="bg-white"></div>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="grid grid-rows-[35.28%_64.72%] h-screen">
+                <div className="grid grid-rows-2 h-full">
+                    <div className="p-1 flex items-center text-8xl font-semibold bg-white">
+                        <h1>
+                            <span className="text-shadow text-pwblue">AI Study</span>
+                            <span className="text-shadow text-pwred"> Companion</span>
+                        </h1>
+                    </div>
+                    <div className="p-1 flex items-center text-8xl font-semibold bg-pwblue">
+                        <h1 className="text-white">Welcome</h1>
+                    </div>
+                </div>
+                <div className="bg-white"></div>
+            </div>
+        </>
+    )
 }
 
 export default App
