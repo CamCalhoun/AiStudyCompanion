53b1c3816b1152b619332a1d6dcf48b761ec67ac
Cameron Calhoun <cameron.calhoun01@gmail.com>
2025-02-19 20:57:02 -0500

    frontend index visually created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './styles.css'
+import Button from './Button.jsx'
+import Card from './Card.jsx'
 function App() {
 
     return (
         <>
-            <div className="grid grid-rows-[35.28%_64.72%] h-screen">
+            <div className="grid grid-rows-[35.28%_64.72%] h-screen bg-bgwhite">
                 <div className="grid grid-rows-2 h-full">
-                    <div className="p-1 flex items-center text-8xl font-semibold bg-white">
+                    <div className="p-1 flex items-center justify-between text-8xl font-semibold">
                         <h1>
                             <span className="text-shadow text-pwblue">AI Study</span>
                             <span className="text-shadow text-pwred"> Companion</span>
                         </h1>
+                        <img
+                            src="pennwest-california.png"
+                            className=""
+                        />
                     </div>
                     <div className="p-1 flex items-center text-8xl font-semibold bg-pwblue">
                         <h1 className="text-white">Welcome</h1>
                     </div>
                 </div>
-                <div className="bg-white"></div>
+
+                <div className="grid grid-cols-[53.75%_46.25%]">
+                    <div className="flex items-center justify-center">
+                        <div className="grid grid-cols-2 gap-20 w-full h-full p-20">
+                            <Button text="Study" />
+                            <Button text="Flashcards" />
+                            <Button text="Import" />
+                            <Button text="Export" />
+                            <Button text="Subjects" />
+                            <Button text="About" />
+                        </div>
+                    </div>
+                    <div className='flex items-center justify-center'>
+                        <Card text="Make a selection on the left to begin!" />
+                    </div>
+                </div>
             </div>
         </>
     )
---
+++ frontend/src/Card.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+function Card({ text }) {
+
+    return (
+        <div className="w-[82.99%] h-[71.77%] py-50 bg-pwblue rounded-xl shadow-xl flex items-center justify-center py-3">
+            <h1 className="text-shadow text-4xl font-bold text-[#F3F4F6]">{text}</h1>
+        </div>
+    )
+}
+
+export default Card
+
