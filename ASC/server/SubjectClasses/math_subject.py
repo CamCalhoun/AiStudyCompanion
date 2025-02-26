2b4b44259b70e19aaffa783eefdc2b885507f096
Seth Morgan <70118609+Mmmtasty@users.noreply.github.com>
2025-02-26 15:58:27 -0500

    Update math_subject.py
    ELOs added for every available field of mathematics. Necessary subject class methods overridden to support multiple ELOs. Improved code formatting.

--- ASC/server/SubjectClasses/math_subject.py
+++ ASC/server/SubjectClasses/math_subject.py
@@ -3,14 +3,36 @@ from typing import List, Dict, ClassVar
 from subject import Subject
 
 class Math(Subject):
-    # Override subject variables
-    subjectName: str = 'Math'
-    subjectElo: int = 600
-    topic: str = "algebra"
-    subtopic: str = "any"
-
-    # Class level attributes
+    subjectName:str = 'Math'
+    subjectElo:int = 600
     subjectBreakpoints: ClassVar[List[int]] = [0, 200, 400, 600, 1000, 1400]
+    subjectPrompts: ClassVar[Dict[int, str]] = {
+        0: 'Very Basic level {topic} problem about {subtopic}',
+        200: 'Basic level {topic} problem about {subtopic}',
+        400: ' Pre-Intermediate level {topic} problem about {subtopic}',
+        600: 'Intermediate level {topic} problem about {subtopic}',
+        1000: 'Advanced level {topic} problem about {subtopic}',
+        1400: 'Expert level {topic} problem about {subtopic}'
+    }
+    currentPrompt: str = ""
+
+    # class level attributes
+
+    topic:str = "algebra"
+
+    subtopic:str = "any"
+
+    topicElo: Dict[str, int] = { # With this a user can be an expert in basic math
+        "basic math" : 600,      # without also being an expert in every other field of math.
+        "pre-algebra": 600,
+        "algebra": 600,
+        "trigonometry": 600,
+        "precalculus": 600,
+        "calculus": 600,
+        "statistics": 600,
+        "linear algebra": 600      
+    }
+
     mathTopics: ClassVar[Dict[str, List[str]]] = {
         "basic math": ["any","addition", "subtraction", "multiplication", "division"],
         "pre-algebra": ["any","fractions", "decimals", "negative numbers", "order of operations"],
@@ -21,47 +43,62 @@ class Math(Subject):
         "statistics": ["any","mean, median, mode", "probability", "standard deviation", "hypothesis testing"],
         "linear algebra": ["any","vectors", "matrices", "determinants", "eigenvalues and eigenvectors"]
     }
-    subjectPrompts: ClassVar[Dict[int, str]] = {
-        0: 'Very Basic level {topic} problem about {subtopic}',
-        200: 'Basic level {topic} problem about {subtopic}',
-        400: ' Pre-Intermediate level {topic} problem about {subtopic}',
-        600: 'Intermediate level {topic} problem about {subtopic}',
-        1000: 'Advanced level {topic} problem about{subtopic}',
-        1400: 'Expert level {topic} problem about {subtopic}'
-    }
 
-    # Instance level initialization
-    currentPrompt: str = ""
+
+
 
     def __init__(self):
-        # Pydantic will handle the fields, so we don't need to set them in __init__ for validation
-        super().__init__()  # Call Pydantic's init if you want to use its functionality
-        
-        # Call the method to update the prompt
         self.updatePrompt()
 
+    def setSubtopic(self, subtopicName):
+        for key, value in self.mathTopics.items():
+            if subtopicName in value: #if we have a valid subtopic it will set the subtopic correctly and also set the topic.
+                self.subtopic = subtopicName
+                self.topic = key 
+                self.subjectElo = self.topicElo[self.topic]
+                self.updatePrompt()
+                return
+            elif subtopicName == key: #"Subtopic entered was actually a topic. So we set the topic with the given name and subtopic set to default 'any'
+                self.topic = key        
+                self.subtopic = "any"
+                self.subjectElo = self.topicElo[self.topic]
+                self.updatePrompt()
+                return
+        # If the subtopic is not in the values or keys of mathTopics
+        raise ValueError("Error: subtopic chosen is not valid.")
+
+    # seTopicElo is designed for changing an Elo that is not the current topic
+    def setTopicElo(self, topicName: str, amount: int):
+        if topicName in self.topicElo.keys():
+            newElo = max(0, min(self.topicElo[topicName] + amount, 1600))
+            self.topicElo[topicName] = newElo
+        else:
+            raise ValueError(f"Invalid topic: {topicName}")
+
+    def getSubtopic(self):
+        return self.subtopic
+
+    def getTopic(self):
+        return self.topic
+               
+            
+    # ############################################################################################################################# #
+    # The following methods are from the Subject class. I am overriding them here because of the various topics associated with mathmatics. #
+    # ############################################################################################################################# #
     def updatePrompt(self):
         # Find the highest valid breakpoint that is <= subjectElo
         validBreakpoints = [bp for bp in self.subjectBreakpoints if bp <= self.subjectElo]
         closestBreakpoint = max(validBreakpoints, default=0)
 
         # Get the corresponding prompt and format it with the topic
-        self.currentPrompt = self.subjectPrompts.get(closestBreakpoint, "").format(topic=self.topic)
+        self.currentPrompt = self.subjectPrompts.get(closestBreakpoint, "").format(topic=self.topic, subtopic=self.subtopic)
 
-    def setSubtopic(self, subtopicName):
-        for key, value in self.mathTopics.items():
-            if subtopicName == value: #if we have a valid subtopic
-                self.subtopic=value
-                self.topic=key 
-                return
-            elif subtopicName == key: #"Subtopic entered was actually a topic. So we set the topic with the given name and subtopic set to default 'any'
-                self.topic=key
-                self.subtopic="any"
-                return
-        # If the subtopic is not in the values or keys of mathTopics
-        print("Error: subtopic chosen is not valid.")
-
+    def setSubjectElo(self, amount: int): 
+        # Send a positive integer to increase, negative integer to decrease.
+        newElo = self.subjectElo + amount
+        self.topicElo[self.topic] = newElo 
+        self.subjectElo = max(0, min(newElo, 1600))
+        self.updatePrompt()
         
+   
 
-                
-            
