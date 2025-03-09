54c0396d92798e8de93c0c0f32d556ff8e4e25eb
Cameron Calhoun <cameron.calhoun01@gmail.com>
2025-03-08 20:15:37 -0500

    adjusted .env name

--- ASC/src/config/api.js
+++ ASC/src/config/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.API_URL || "http://127.0.0.1:8000"
+const BASE_URL = process.env.VITE_API_URL || "http://127.0.0.1:8000"
 
 export const API_HELLO = `${BASE_URL}/api/hello`
 export const API_EXPORT = `${BASE_URL}/api/export`
